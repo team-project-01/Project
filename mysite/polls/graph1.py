d8dccdc033777cc7dfbc56b10cda4324260d96c8
woosuha <wsh120@naver.com>
2024-04-15 18:55:55 +0900

    Update graph1.py

--- mysite/polls/graph1.py
+++ mysite/polls/graph1.py
@@ -3,19 +3,27 @@
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] ='Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] =False
-#기온-x, 풍속-y, 강수량-z 
+#기온, 풍속, 강수량
+#오늘 1일전 2일전 일주일전
+x = ['7일전','2일','1일전','오늘']
+a1 = [10,20,50,20]         
+a2 = [30,20,40,10]
+a3 = [30,20,10,40]    
 
-x = [10,20,30,40,50,60,70,80]         
-y = [30,40,60,20,10,40,20,70]
-z = [20,40,50,80,60,90,10,20]    
+fig, axs = plt.subplots(1,3)
 
-plt.xlabel('연간')         
-plt.ylabel('온도')          
-plt.title('그래프')   
-plt.plot(x, x, label = '기온')   
-plt.plot(x, y, label = '풍속')
-plt.plot(x, z, label = '강수량')
+fig.suptitle('Weather',fontweight ="bold", fontsize=20) 
 
-plt.legend()                   
+axs[0].plot(x, a1, label = '기온', color = 'r')  
+axs[0].legend()
+axs[0].set_ylim(5,65)
+
+axs[1].plot(x, a2, label = '풍속', color = 'g')
+axs[1].legend()
+
+axs[2].plot(x, a3, label = '강수량', color = 'b')
+axs[2].legend()
+fig.tight_layout()
+
 plt.show()
 # %%
