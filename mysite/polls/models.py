b4a751bce63611885176eea4a286b87118efd131
junghwa0609 <hayan094666@gmail.com>
2024-04-16 13:53:05 +0900

    날씨 저장까지 일단 구현
    ~~8000/weather 페이지 갔다오면
    api정보를 수집해와서
    필드에 바로 넣어주는 코드 구현(아직 단기예보만 적용함. 이제 과거 예보들도 가져와서 할 예정)

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,29 @@
+
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class forecastData(models.Model): #날짜 별 온도 데이터 저장
+    fcstDate = models.CharField(max_length=8)
+    fcstTime = models.CharField(max_length=2)
+    fcstValue = models.CharField(max_length=10)
+    def __str__(self):
+        return f'{self.fcstDate[:4]}년 {self.fcstDate[4:6]}월 {self.fcstDate[6:]}일 {self.fcstTime}시 온도(도씨)'
+    
+class Rainpercent(models.Model): #날짜 별 강수확률 데이터 저장
+    fcstDate = models.CharField(max_length=8)
+    fcstTime = models.CharField(max_length=2)
+    fcstValue = models.CharField(max_length=10)
+    def __str__(self):
+        return f'{self.fcstDate[:4]}년 {self.fcstDate[4:6]}월 {self.fcstDate[6:]}일 {self.fcstTime}시 강수확률(%)'
+    
+class Wind(models.Model): #날짜 별 풍속 데이터 저장
+    fcstDate = models.CharField(max_length=8)
+    fcstTime = models.CharField(max_length=2)
+    fcstValue = models.CharField(max_length=10)
+    def __str__(self):
+        return f'{self.fcstDate[:4]}년 {self.fcstDate[4:6]}월 {self.fcstDate[6:]}일 {self.fcstTime}시 풍속(m/s)'
+
 
